debf71b32c33fa6b01b56a62ba063fbb9322db09
Trevor Pierce <trevorp@uneelabs.com>
2019-07-17 11:46:02 -0400

    Fixed issues with bubble chart. Added new verbiage for  digital distraction

--- src/app/trends/content/trend3/trend3.component.ts
+++ src/app/trends/content/trend3/trend3.component.ts
@@ -1,5 +1,5 @@
 import { ViewportScroller } from '@angular/common';
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, DoCheck, AfterViewChecked } from '@angular/core';
 import { TranslationService } from '../../../shared/translation.service';
 import { trigger, transition, style, animate } from '@angular/animations';
 
@@ -16,16 +16,36 @@ import { trigger, transition, style, animate } from '@angular/animations';
         ])
     ]
 })
-export class Trend3Component {
+export class Trend3Component implements AfterViewChecked {
+
+
 
     public constructor(public translationService: TranslationService,
         private viewScroller: ViewportScroller) {
     }
 
+    @ViewChild('bubbleDialog') bubbleDialog: ElementRef<HTMLElement>;
+
     public scrollTo(tag: string) {
 
         this.viewScroller.scrollToAnchor(tag);
 
+    }
+
+    private topAdjusted: boolean = false;
+
+    ngAfterViewChecked() {
+        if (!this.topAdjusted) {
+            setTimeout(() => {
+                this.topAdjusted = true;
+                console.log(this.bubbleDialog);
+                console.log('Dialog Height: ' + this.bubbleDialog.nativeElement.clientHeight);
+                if (this.expanded) {
+                    this.top -= this.bubbleDialog.nativeElement.clientHeight / 2;
+                }
+
+            });
+        }
     }
 
     public bubbles: any[] = [{
@@ -37,7 +57,7 @@ export class Trend3Component {
         icon: "/assets/mobile/bubble_2_security.png",
         title: "trend_3_bubble_security_title",
         callout: "trend_3_bubble_security_callout"
-    },{
+    }, {
         icon: "/assets/mobile/bubble_3_distraction.png",
         title: "trend_3_bubble_distraction_title",
         callout: "trend_3_bubble_distraction_callout"
@@ -50,18 +70,20 @@ export class Trend3Component {
         title: "trend_3_bubble_bias_title",
         callout: "trend_3_bubble_bias_callout"
     }];
-    
+
     public expanded: boolean = false;
     public title: string;
     public callout: string;
     public left: number = 0;
     public top: number = 0;
 
-    zoneClick(event) {
+
+    zoneClick(event, bubbleDialog: HTMLElement) {
         if (this.translationService.data) {
+            this.topAdjusted = false;
             this.title = this.translationService.data[event.target.target + "_title"];
             this.callout = this.translationService.data[event.target.target + "_callout"];
-            this.left = event.layerX;
+            this.left = event.layerX - 100;
             this.top = event.layerY;
             this.expanded = true;
         }

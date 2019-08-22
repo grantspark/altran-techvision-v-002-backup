82faaeb25343d5b147684d1e025c0d349ed72fb2
Trevor Pierce <trevorp@uneelabs.com>
2019-08-22 15:42:42 -0400

    testing another method of modifying screen height

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,15 @@
 import { transition, trigger, useAnimation } from '@angular/animations';
-import { Component, OnInit }                 from '@angular/core';
-import { fadeInLeft }                        from 'ng-animate';
-import { HeaderService }                     from '../_layout/header/header.service';
-import { TranslationService }                from '../shared/translation.service';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { fadeInLeft } from 'ng-animate';
+import { HeaderService } from '../_layout/header/header.service';
+import { TranslationService } from '../shared/translation.service';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
-    styleUrls: [ './home.component.scss' ],
+    styleUrls: ['./home.component.scss'],
     animations: [
-        trigger('fadeInLeft', [ transition('* => *', useAnimation(fadeInLeft), {
+        trigger('fadeInLeft', [transition('* => *', useAnimation(fadeInLeft), {
 
             params: {
 
@@ -17,24 +17,27 @@ import { TranslationService }                from '../shared/translation.service
 
             }
 
-        }) ])
+        })])
     ],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit {
 
     public fadeInLeft: any;
+    @ViewChild('content', { static: false })
+    elementView: ElementRef;
 
     public constructor(private headerService: HeaderService,
-                       public translationService: TranslationService) {
+        public translationService: TranslationService) {
 
         headerService.showBack = false;
 
     }
 
-    ngOnInit() {
-        var height=document.getElementsByTagName("html")[0].scrollHeight;
+    ngAfterViewInit() {
+        var height = this.elementView.nativeElement.offsetHeight;
+
         console.log("Height: " + height);
-        window.parent.postMessage({"height": height}, "*");
+        window.parent.postMessage({ "height": height }, "*");
     }
 
 }
